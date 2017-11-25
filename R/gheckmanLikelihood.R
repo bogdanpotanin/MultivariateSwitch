072865991b267065f172dbe592394f12ffe2c2a4
bogdanpotanin <bogdanpotanin@gmail.com>
2017-11-25 12:06:29 +0300

    123

--- R/gheckmanLikelihood.R
+++ R/gheckmanLikelihood.R
@@ -318,16 +318,3 @@ gheckmanGradient<-function(x, y, zh, yh, zo, ns, ndz, nSigma, coef, group, ngrou
 {
   return(gheckmanLikelihood(x, y, zh, yh, zo, ns, ndz, nSigma, coef, group, ngroup, nsMax, zo3Converter, noutcome, zo3, groupsize, nrhoY, ShowInfo=FALSE, maximization=FALSE)[[2]])
 }
-
-#Memoised function
-gheckmanLikelihood1=memoise(gheckmanLikelihood)
-
-gheckmanF=function(x, y, zh, yh, zo, ns, ndz, nSigma, coef, group, ngroup, nsMax, zo3Converter, noutcome, zo3, groupsize, nrhoY, ShowInfo=TRUE, maximization=FALSE)
-{
-  return(gheckmanLikelihood1(x, y, zh, yh, zo, ns, ndz, nSigma, coef, group, ngroup, nsMax, zo3Converter, noutcome, zo3, groupsize, nrhoY, ShowInfo, maximization)[[1]])
-}
-
-gheckmanG=function(x, y, zh, yh, zo, ns, ndz, nSigma, coef, group, ngroup, nsMax, zo3Converter, noutcome, zo3, groupsize, nrhoY, ShowInfo=TRUE, maximization=FALSE)
-{
-  return(gheckmanLikelihood1(x, y, zh, yh, zo, ns, ndz, nSigma, coef, group, ngroup, nsMax, zo3Converter, noutcome, zo3, groupsize, nrhoY, ShowInfo, maximization)[[2]])
-}
